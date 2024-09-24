724ce3b23e55a750118cd3c47dbf857f12b9bc00
𝐄𝐱𝐜𝐞𝐩𝐭𝐢𝐨𝐧𝐚𝐥 𝐛𝐨𝐲 <120129255+mahdimirbagheri@users.noreply.github.com>
2024-09-24 11:25:58 +0330

    Delete Conditionals directory

--- Conditionals/bank/bank.py
+++
@@ -1,8 +0,0 @@
-dollars = 0
-greet = input("Greeting: ").strip().upper()
-if greet.startswith("HELLO"):
-    print(f"${dollars}")
-elif greet.startswith("HOW"):
-    print(f"${dollars + 20}")
-elif greet.startswith("WHAT"):
-    print(f"${dollars + 100}")
--- Conditionals/deep/deep.py
+++
@@ -1,5 +0,0 @@
-number = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-if number.strip() == "42" or number.lower().strip() == "forty-two" or number.lower().strip() == "forty two":
-    print("Yes")
-else:
-    print("No")
